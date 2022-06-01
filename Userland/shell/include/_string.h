bfda64b6c314bb0e9f642319ab258d111ca80057
Cuinardium <sballerini@itba.edu.ar>
2022-06-01 09:02:39 -0300

    added a string parser

--- Userland/shell/include/_string.h
+++ Userland/shell/include/_string.h
@@ -7,7 +7,11 @@
 uint64_t strlen(const char * str);
 int64_t strncmp(const char * str1, const char * str2, uint64_t n);
 int64_t strcmp(const char * str1, const char * str2);
+<<<<<<< Updated upstream
 int64_t strParse(const char * str, char * buff1, char * buff2, char divider);
 int64_t strtol(const char *str, const char **endptr, int base);
+=======
+int64_t strDivide(const char * str, char * buff1, char * buff2, char divider);
+>>>>>>> Stashed changes
 
 #endif//_STRING_H
